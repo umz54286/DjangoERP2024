148d16a91df3d5d204271907dac65d6840030e6d
Yufang <umz54286@gmail.com>
2024-10-25 15:37:50 +0800

    setting.py add ALLOWED_HOSTS and set staticfiles

--- DjangoERP/settings.py
+++ DjangoERP/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-0j7_qz-vyw-eqbz6^$rfhbpqgttv9%62a8%m)+b^h%a(7n-(wg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoerp.onrender.com']
 
 
 # Application definition
@@ -128,6 +129,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
