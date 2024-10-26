f8c696217a01a1415f76ba610e22655a30a3911b
Yufang <umz54286@gmail.com>
2024-10-26 17:31:00 +0800

    change staticfile setting

--- DjangoERP/settings.py
+++ DjangoERP/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0j7_qz-vyw-eqbz6^$rfhbpqgttv9%62a8%m)+b^h%a(7n-(wg"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['djangoerp.onrender.com','127.0.0.1']
 
@@ -125,11 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 
 # Default primary key field type
